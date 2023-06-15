f5dba70507ba37fb1b2a7e20fc5f382a5d86837b
Sergio Andrés Jaimes Pimiento <sjaimes16@outlook.com>
2023-06-15 15:46:25 -0500

    agregado cambios en componente

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 
 const Pizza = ({ tiendaPizza }) => {
-  const [cantidadRebanadas, setCantidadRebanadas] = useState(8);
+  const [cantidadRebanadas, setCantidadRebanadas] = useState(new Array(tiendaPizza.length).fill(8));
 
-  const comerRebanada = () => {
-    if (cantidadRebanadas > 0) {
-      setCantidadRebanadas(cantidadRebanadas - 1);
+  const comerRebanada = (index) => {
+    if (cantidadRebanadas[index] > 0) {
+      const newCantidadRebanadas = [...cantidadRebanadas];
+      newCantidadRebanadas[index] -= 1;
+      setCantidadRebanadas(newCantidadRebanadas);
     }
   };
 
-  const sumarRebanada = () => {
-    if (cantidadRebanadas < 8) {
-      setCantidadRebanadas(cantidadRebanadas + 1);
+  const sumarRebanada = (index) => {
+    if (cantidadRebanadas[index] < 8) {
+      const newCantidadRebanadas = [...cantidadRebanadas];
+      newCantidadRebanadas[index] += 1;
+      setCantidadRebanadas(newCantidadRebanadas);
     }
   };
 
@@ -22,21 +26,21 @@ const Pizza = ({ tiendaPizza }) => {
           <div className="box">
             <div className="content">
               <h2>01</h2>
-              <img src={pizza.image} alt="Pizza" width={200} height={200}/>
+              <img src={pizza.image} alt="Pizza" width={200} height={200} />
               <h3>{pizza.nombre}</h3>
-                <h3>Ingredientes: {pizza.ingredientes}</h3>
-                <h3>Precio: {pizza.precio}</h3>
-                <h3>Cantidad: {cantidadRebanadas}</h3>
-              
-                {cantidadRebanadas > 0 && (
-                    <a onClick={comerRebanada}>Ordenar</a>
-                )}
-                {cantidadRebanadas === 0 && (
+              <h3>Ingredientes: {pizza.ingredientes}</h3>
+              <h3>Precio: {pizza.precio}</h3>
+              <h3>Cantidad: {cantidadRebanadas[index]}</h3>
+
+              {cantidadRebanadas[index] > 0 && (
+                <a onClick={() => comerRebanada(index)}>Ordenar</a>
+              )}
+              {cantidadRebanadas[index] === 0 && (
                 <div>
-                    <p>¡Se acabaron las rebanadas!</p>
-                    <a onClick={sumarRebanada}>Agregar rebanada</a>
+                  <p>¡Se acabaron las rebanadas!</p>
+                  <a onClick={() => sumarRebanada(index)}>Agregar rebanada</a>
                 </div>
-                )}
+              )}
             </div>
           </div>
         </div>
